52fbcaa3270027185e6377adee48749cbed1b09a
Gopal Chavan <gopal4chavan@gmail.com>
2024-10-04 19:42:00 +0530

    participant static method

--- src/models/participants.js
+++ src/models/participants.js
@@ -9,7 +9,19 @@ const ParticipantSchema = new Schema({
   score: { type: Number, default: 0 },
   type: { type: String, enum: ['observer', 'player'], required: true },
   roomId: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
-})
+});
+
+
+ParticipantSchema.statics.createParticipant = async function (participantData) {
+  try {
+    const participant = new this(participantData); // 'this' refers to the model
+    return await participant.save();
+  } catch (error) {
+    console.error('Error creating participant:', error);
+    throw error; // Re-throw the error for handling outside
+  }
+};
+
 
 const model = mongoose.model('Participant', ParticipantSchema)
 module.exports = model
