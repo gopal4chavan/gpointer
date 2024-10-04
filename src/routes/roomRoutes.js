9f80999eaaad0e2fb69df7b8c122f4effff7732b
Gopal Chavan <gopal4chavan@gmail.com>
2024-10-04 19:28:23 +0530

    updating host with userid

--- src/routes/roomRoutes.js
+++ src/routes/roomRoutes.js
@@ -8,9 +8,9 @@ const { addNewRoom } = require('../helper/redisMongo');
 // Endpoint to create a room
 router.post('/rooms', async (req, res) => {
     const roomName = req.body.name
-    const host = req.user
+    const host = req.user._id
     try {
-        const roomId = await addNewRoom(req.body.name); // Use req.body for POST data
+        const roomId = await addNewRoom(roomName, host); // Use req.body for POST data
         res.status(201).json({ roomId });
     } catch (error) {
         res.status(500).json({ error: 'Failed to create room' });
