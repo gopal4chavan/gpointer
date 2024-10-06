5466414c6369d9d75ee617429c0f8495d5ee603f
Gopal Chavan <gopal4chavan@gmail.com>
2024-10-06 18:11:01 +0530

    minor updates on pushing participants to redis

--- src/routes/roomRoutes.js
+++ src/routes/roomRoutes.js
@@ -1,8 +1,7 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 const router = express.Router();
-
 const { redisPushParticipants, redisGetUserSocketId } = require('../redis');
 const { createRoom } = require('../services/roomService')
 const Participant = require('../models/participants')
@@ -12,30 +11,44 @@ const { getIOInstance } = require('../socket')
 router.post('/rooms', async (req, res) => {
   const roomName = req.body.name
   const host = req.user._id
+
   try {
     const room = await createRoom(roomName, host)
+
     const newParticipantData = {
-      name: user.name,
+      name: req.user.name,
       status: 'active',
       score: 0,
       type: 'player',
-      roomId: room._id
-    };
-    Participant.createParticipant(newParticipantData).then(async participant => {
-      redisPushParticipants(room._id, participant)
-      const socketId = await redisGetUserSocketId(req.user.name)
-      const io = getIOInstance()
-      const socket = io.sockets.sockets.get(socketId);
-      io.to(socket).emit('registerUser')
-    }).catch((error) => {
-      // Handle any errors that occur during participant creation
-      console.error('Error creating participant:', error);
-    });
+      roomId: room._id,
+    }
 
-    redisPushParticipants(room._id, participant)
+    // Create participant in the background
+    Participant.createParticipant(newParticipantData)
+      .then(async (participant) => {
+        // Get user socket ID and emit event in the background
+        const socketId = await redisGetUserSocketId(req.user.name);
+        const io = getIOInstance();
+        const socket = io.sockets.sockets.get(socketId);
 
-    res.status(201).json({ roomId });
+        if (socket) {
+          socket.emit('joinRoom')
+          participant.socketId = socketId
+          // Push participant to Redis in the background
+          await redisPushParticipants(room._id, participant);
+        } else {
+          console.warn('Socket not found for user:', req.user.name);
+        }
+      })
+      .catch((error) => {
+        console.error('Error creating participant:', error);
+        // You can choose to log the error without affecting the response
+      });
+
+    // Respond immediately with the room ID
+    res.status(201).json({ roomId: room._id });
   } catch (error) {
+    console.error('Failed to create room:', error);
     res.status(500).json({ error: 'Failed to create room' });
   }
 });
